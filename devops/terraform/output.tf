c2061df8a1954c3294df9693f32faf2ce087a7b4
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-17 13:36:43 +0100

    update output.tf

--- devops/terraform/output.tf
+++ devops/terraform/output.tf
@@ -5,10 +5,12 @@ output "storage_account_name_list" {
 
 output "storage_account_primary_connection_string_list" {
     value = [for storage_account in azurerm_storage_account.this : storage_account.primary_connection_string]
+    sensitive = true
 }
 
 output "storage_account_primary_access_key_list" {
     value = [for storage_account in azurerm_storage_account.this : storage_account.primary_access_key]
+    sensitive = true
 }
 
 output "storage_account_container_name_list" {
